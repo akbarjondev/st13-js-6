dc45d7d0193e7566d242d55c56bf3731d56c2522
Akbarjondev <tojiyev.akbarjon@gmail.com>
2021-09-09 19:48:36 +0500

    Add vazifa

--- js/main.js
+++ js/main.js
@@ -9,4 +9,21 @@ var yevro = 10354.03;
 
 var som_xarajat = (borish_kelish_dollar + mehmonxona_dollar) * dollar + muzey_yevro * yevro;
 
-console.log(som_xarajat);
+// console.log(som_xarajat);
+
+var user_som = Number(prompt("Qancha puling bor?")); // agar user harflarni kiritsa uni numberlasak NaN
+
+// NaN - if kelganda false qaytaradi
+// falsy qiymatlar: NaN, undefined, null, '', 0, false
+
+if (user_som == 0 || user_som == NaN) {
+  if (user_som >= som_xarajat) {
+    alert("Oq yo'l");
+  } else {
+    console.log("Uzr ukam (")
+  }
+} else {
+  alert("turi qiymat kirit");
+}
+
+
