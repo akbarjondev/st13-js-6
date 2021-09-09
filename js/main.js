1cfa6e56fb399a46ca6ed028c60520adadda1c4c
Akbarjondev <tojiyev.akbarjon@gmail.com>
2021-09-09 19:33:07 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// xarajatlar
+var borish_kelish_dollar = 500;
+var mehmonxona_dollar = 250;
+var muzey_yevro = 120;
+
+// kurs
+var dollar = 9433.34;
+var yevro = 10354.03;
+
+var som_xarajat = (borish_kelish_dollar + mehmonxona_dollar) * dollar + muzey_yevro * yevro;
+
+console.log(som_xarajat);
